eb537e586d5875f1e86086e26603b81c068bae1f
Somnath Swami <73718404+Somnath-swami@users.noreply.github.com>
2021-04-25 16:08:09 +0530

    Update Selection Sort.c

--- Searching and Sorting technics/Sorting technics/Selection Sort.c
+++ Searching and Sorting technics/Sorting technics/Selection Sort.c
@@ -1,1 +1,48 @@
+#include<stdio.h>
 
+int swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+int SelectionSort(int a[], int n)
+{
+    int i, j, min_ind;
+    for(i=0;i<n-1;i++)
+    {
+        min_ind = i;
+        for(j=i+1;j<n;j++)
+            if(a[j]<a[min_ind])
+        
+        min_ind = j;
+        
+    swap(&a[min_ind], &a[i]);
+    }
+}
+
+int PrintArray(int a[], int n)
+{
+    for(int i=0;i<n;i++)
+        printf("%d ",a[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int n;
+    printf("Enter the size of array = ");
+    scanf("%d",&n);
+    
+    int a[n];
+    printf("Enter %d elements in array \n",n);
+    for(int i=0;i<n;i++)
+        scanf("%d",&a[i]);
+        
+    SelectionSort(a, n);
+    
+    PrintArray(a, n);
+    
+    return 0;
+}
